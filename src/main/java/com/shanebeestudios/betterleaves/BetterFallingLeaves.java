c47308055730e3b627fb8683a6ffacb0790aad50
ShaneBeee <shanebolenback@me.com>
2026-04-28 14:19:59 -0700

    FallingLeavesMixin - add toggle and countdown to remove particle

--- src/main/java/com/shanebeestudios/betterleaves/BetterFallingLeaves.java
+++ src/main/java/com/shanebeestudios/betterleaves/BetterFallingLeaves.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ServerboundChangeGameModePacket;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.GameType;
@@ -18,6 +19,7 @@ public class BetterFallingLeaves implements ClientModInitializer {
 
 	public static final String MOD_ID = "betterfallingleaves";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static boolean IS_BETTER_FALLING_LEAVES = true;
 
 	@Override
 	public void onInitializeClient() {
@@ -25,25 +27,39 @@ public class BetterFallingLeaves implements ClientModInitializer {
 		registerKeybind();
 	}
 
-	private static KeyMapping keyBinding;
+	private static KeyMapping gamemodeSwitchKeybind;
+	private static KeyMapping betterFallingKeybind;
 	private static KeyMapping.Category CATEGORY;
 
 	private static void registerKeybind() {
 		CATEGORY = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "title"));
-		keyBinding = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+		gamemodeSwitchKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
 			"key.betterfallingleaves.gamemode_change",
 			InputConstants.Type.KEYSYM,
 			GLFW.GLFW_KEY_O, CATEGORY));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (keyBinding.consumeClick()) {
-				LocalPlayer player = client.player;
-				assert player != null;
+		betterFallingKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+			"key.betterfallingleaves.better_leaves",
+			InputConstants.Type.KEYSYM,
+			GLFW.GLFW_KEY_M, CATEGORY));
 
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			LocalPlayer player = client.player;
+			assert player != null;
+			if (gamemodeSwitchKeybind.consumeClick()) {
 				GameType playerMode = player.gameMode();
 				ServerboundChangeGameModePacket packet = new ServerboundChangeGameModePacket(
 					playerMode == GameType.CREATIVE ? GameType.SURVIVAL : GameType.CREATIVE);
 				player.connection.send(packet);
+			}
+			if (betterFallingKeybind.consumeClick()) {
+				if (IS_BETTER_FALLING_LEAVES) {
+					IS_BETTER_FALLING_LEAVES = false;
+					player.sendSystemMessage(Component.literal("Better Falling Leaves toggled off!"));
+				} else {
+					IS_BETTER_FALLING_LEAVES = true;
+					player.sendSystemMessage(Component.literal("Better Falling Leaves toggled on!"));
+				}
 			}
 		});
 	}
--- src/main/java/com/shanebeestudios/betterleaves/mixin/FallingLeavesMixin.java
+++ src/main/java/com/shanebeestudios/betterleaves/mixin/FallingLeavesMixin.java
@@ -1,5 +1,6 @@
 package com.shanebeestudios.betterleaves.mixin;
 
+import com.shanebeestudios.betterleaves.BetterFallingLeaves;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.FallingLeavesParticle;
 import net.minecraft.client.particle.Particle;
@@ -8,6 +9,8 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(FallingLeavesParticle.class)
 public abstract class FallingLeavesMixin extends Particle {
 
+    private int countdown = 20;
+
     protected FallingLeavesMixin(ClientLevel clientLevel, double d, double e, double f) {
         super(clientLevel, d, e, f);
     }
@@ -15,8 +18,16 @@ public abstract class FallingLeavesMixin extends Particle {
     @Override
     public void move(double d, double e, double f) {
         super.move(d, e, f);
-        this.onGround = false;
-        this.hasPhysics = false;
+        if (BetterFallingLeaves.IS_BETTER_FALLING_LEAVES) {
+            if (this.onGround || this.countdown < 20) {
+                this.countdown--;
+                this.onGround = false;
+                this.hasPhysics = false;
+                if (this.countdown <= 0) {
+                    this.remove();
+                }
+            }
+        }
     }
 
 }
