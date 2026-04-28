856e65b54f9ef1442fff5cc7dd7dc2396be17b68
ShaneBeee <shanebolenback@me.com>
2026-04-28 13:45:56 -0700

    Update for MC 26.1.2 - part 2: - fix particle

--- src/main/java/com/shanebeestudios/betterleaves/mixin/FallingLeavesMixin.java
+++ src/main/java/com/shanebeestudios/betterleaves/mixin/FallingLeavesMixin.java
@@ -3,7 +3,6 @@ package com.shanebeestudios.betterleaves.mixin;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.FallingLeavesParticle;
 import net.minecraft.client.particle.Particle;
-import net.minecraft.client.renderer.LightTexture;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(FallingLeavesParticle.class)
@@ -18,11 +17,6 @@ public abstract class FallingLeavesMixin extends Particle {
         super.move(d, e, f);
         this.onGround = false;
         this.hasPhysics = false;
-    }
-
-    @Override
-    protected int getLightColor(float f) {
-        return LightTexture.FULL_BRIGHT;
     }
 
 }
