e1e03ed57e644bab0d98ff51eb0a288ae553ee06
ShaneBeee <shanebolenback@me.com>
2026-04-28 15:31:22 -0700

    BetterFallingLeaves - formatting

--- src/main/java/com/shanebeestudios/betterleaves/BetterFallingLeaves.java
+++ src/main/java/com/shanebeestudios/betterleaves/BetterFallingLeaves.java
@@ -18,53 +18,53 @@ import java.awt.Color;
 
 public class BetterFallingLeaves implements ClientModInitializer {
 
-	public static final String MOD_ID = "betterfallingleaves";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static boolean IS_BETTER_FALLING_LEAVES = true;
+    public static final String MOD_ID = "betterfallingleaves";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static boolean IS_BETTER_FALLING_LEAVES = true;
 
-	@Override
-	public void onInitializeClient() {
-		LOGGER.info("Time for better falling leaves!");
-		registerKeybinds();
-	}
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Time for better falling leaves!");
+        registerKeybinds();
+    }
 
-	private static void registerKeybinds() {
-		KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "title"));
-		KeyMapping gamemodeKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
-			"key.betterfallingleaves.gamemode_change",
-			InputConstants.Type.KEYSYM,
-			GLFW.GLFW_KEY_O, category));
+    private static void registerKeybinds() {
+        KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(MOD_ID, "title"));
+        KeyMapping gamemodeKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+            "key.betterfallingleaves.gamemode_change",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_O, category));
 
-		KeyMapping leavesKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
-			"key.betterfallingleaves.better_leaves",
-			InputConstants.Type.KEYSYM,
-			GLFW.GLFW_KEY_M, category));
+        KeyMapping leavesKeybind = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+            "key.betterfallingleaves.better_leaves",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_M, category));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			LocalPlayer player = client.player;
-			assert player != null;
-			if (gamemodeKeybind.consumeClick()) {
-				GameType previousMode = player.gameMode();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            LocalPlayer player = client.player;
+            assert player != null;
+            if (gamemodeKeybind.consumeClick()) {
+                GameType previousMode = player.gameMode();
 
-				GameType newMode = previousMode == GameType.CREATIVE ? GameType.SURVIVAL : GameType.CREATIVE;
-				if (player.isCrouching()) {
-					newMode = GameType.SPECTATOR;
-				}
-				ServerboundChangeGameModePacket packet = new ServerboundChangeGameModePacket(newMode);
-				player.connection.send(packet);
-			}
-			if (leavesKeybind.consumeClick()) {
-				if (IS_BETTER_FALLING_LEAVES) {
-					IS_BETTER_FALLING_LEAVES = false;
-					player.sendSystemMessage(Component.literal("Better Falling Leaves toggled off!")
-						.withColor(new Color(196, 7, 7).getRGB()));
-				} else {
-					IS_BETTER_FALLING_LEAVES = true;
-					player.sendSystemMessage(Component.literal("Better Falling Leaves toggled on!")
-						.withColor(new Color(71, 201, 8).getRGB()));
-				}
-			}
-		});
-	}
+                GameType newMode = previousMode == GameType.CREATIVE ? GameType.SURVIVAL : GameType.CREATIVE;
+                if (player.isCrouching()) {
+                    newMode = GameType.SPECTATOR;
+                }
+                ServerboundChangeGameModePacket packet = new ServerboundChangeGameModePacket(newMode);
+                player.connection.send(packet);
+            }
+            if (leavesKeybind.consumeClick()) {
+                if (IS_BETTER_FALLING_LEAVES) {
+                    IS_BETTER_FALLING_LEAVES = false;
+                    player.sendSystemMessage(Component.literal("Better Falling Leaves toggled off!")
+                        .withColor(new Color(196, 7, 7).getRGB()));
+                } else {
+                    IS_BETTER_FALLING_LEAVES = true;
+                    player.sendSystemMessage(Component.literal("Better Falling Leaves toggled on!")
+                        .withColor(new Color(71, 201, 8).getRGB()));
+                }
+            }
+        });
+    }
 
 }
